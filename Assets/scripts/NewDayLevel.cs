5241e54501afe28b4049d166eac21991d107a69a
Nikita Kazantsev <kaznik2013@gmail.com>
2018-06-16 15:19:19 +0300

    change lvl

---
+++ Assets/scripts/NewDayLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NewDayLevel : MonoBehaviour {
+	public GameObject dialogC;
+	public GameObject LifeIQC;
+	public GameObject TimeC;
+	public GameObject DayEndingC;
+	public SubjectsControl sb;
+	public LIFEandIQ lifeaiq;
+	public void LoadStage()  {
+		sb.DayEnd ();
+		PlayerPrefs.SetInt ("needsnewday", 0);
+
+		Application.LoadLevel ("FirstFloor");
+
+		dialogC.SetActive (true);
+		LifeIQC.SetActive (true);
+		TimeC.SetActive (true);
+		DayEndingC.SetActive (false);
+		sb.DayStart ();
+	}
+}
