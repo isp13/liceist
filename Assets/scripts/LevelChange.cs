6c15d66d319f7d4d1dfca276f161841ee9ef7fb5
Nikita Kazantsev <kaznik2013@gmail.com>
2018-09-05 20:14:07 +0300

    level-change script edited

--- Assets/scripts/LevelChange.cs
+++ Assets/scripts/LevelChange.cs
@@ -7,13 +7,15 @@ using UnityEngine.SceneManagement;
 public class LevelChange : MonoBehaviour {
 
 	public GameObject maincharacter;
+	public int xcoords;
+	public int ycoords;
 
 	[SerializeField] private string loadlevel;
 
 	void OnTriggerEnter2D (Collider2D other)
 	{
 		if (other.CompareTag("Player"))
-			maincharacter.transform.position= Vector3.zero;
+			maincharacter.transform.position = new Vector3(xcoords, ycoords, -10f);
 			//SceneManager.LoadScene(loadlevel);
 	}
 }
