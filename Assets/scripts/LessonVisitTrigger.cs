c2dc4622a8fe44811a930e63e78b2a791b3a3cea
Nikita Kazantsev <kaznik2013@gmail.com>
2018-10-16 20:02:31 +0300

    Minnor bugs fix

--- Assets/scripts/LessonVisitTrigger.cs
+++ Assets/scripts/LessonVisitTrigger.cs
@@ -6,15 +6,33 @@ public class LessonVisitTrigger : MonoBehaviour {
 
 	[SerializeField] private int classID;
 
-	public Button bttn;
-
-
+	public Button TEORP;
+	public Button MATH;
+	public Button INFA;
+	public Button RUSS;
+	public Button PHYSC;
+	public Button LITER;
 
 	void OnTriggerEnter2D (Collider2D other)
 	{
 		if (other.CompareTag("Player") && PlayerPrefs.GetInt("CurrentClass")==classID && PlayerPrefs.GetInt("wasitvisited")!=1 )
 		{
-			bttn.onClick.Invoke ();
+			if (classID==203)
+				TEORP.onClick.Invoke ();
+			if (classID == 205)
+				MATH.onClick.Invoke ();
+			if (classID == 307 || classID == 208 || classID == 207 || classID==501)
+				INFA.onClick.Invoke ();
+			if (classID == 301)
+				RUSS.onClick.Invoke ();
+			if (classID == 209)
+				PHYSC.onClick.Invoke ();
+			if (classID == 403)
+				LITER.onClick.Invoke ();
+			
+				
+				
+				
 			PlayerPrefs.SetInt("visits", PlayerPrefs.GetInt("visits")+1);
 			PlayerPrefs.SetInt("wasitvisited",1);
 			int iqcounter=PlayerPrefs.GetInt ("IQ");
